0ea24017d50c2e65597b8addab801fb999fccb37
brunocalazansca <bcalazanscarritilha@gmail.com>
2026-07-16 22:30:18 -0300

    fix: corrigir problema de acesso a rotas autenticadas com o banco de dados vazio

--- spa/src/service/api.ts
+++ spa/src/service/api.ts
@@ -18,7 +18,17 @@ api.interceptors.request.use(
 
         return config;
     },
+    (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+    (response) => response,
     (error) => {
+        if (error.response?.status === 401) {
+            localStorage.removeItem('token');
+            localStorage.removeItem('user_id');
+            window.location.href = '/login';
+        }
         return Promise.reject(error);
     }
 );
